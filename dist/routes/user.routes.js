32d59fc1997ac3684a6d492d380bfd3865d3c419
user1 <mh.torneria@gmail.com>
2024-06-05 12:04:08 -0400

    quitados los archivos de dist/

--- dist/routes/user.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const user_controller_1 = require("../controllers/user.controller");
-const userRouter = (0, express_1.Router)();
-userRouter.post('/', user_controller_1.createUserHandler);
-exports.default = userRouter;
